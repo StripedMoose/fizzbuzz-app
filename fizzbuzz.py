0847aeb87837661c0b4f07cd158259594f5df5a4
Chris Smith <csmith@circle.com>
2017-03-10 07:49:25 -0500

    fizzbuzz functional

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,15 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.cli.command('test')
-
 def is_fizz(n):
 	if ( n % 3 == 0 ):
-		return True
+		return True 
 
 def is_buzz(n):
 	if ( n % 5 == 0 ):
@@ -18,27 +12,26 @@ def is_buzz(n):
 
 def find_response(x):
 
-	response = "ack"
-
-	if is_fizz(x):
-		response = "fizz"
-
-#	if is_fizz(x) and is_buzz(x):
-#		response = "fizzbuzz"
-#	else:	
-#		if is_fizz(x):
-#			response = "fizz"
-#		elif is_buzz(x):
-#			response = "buzz"
-#		else:
-#			x += 1
-#			resposne = str(x)
+	if is_fizz(x) and is_buzz(x):
+		response = "fizzbuzz"
+	else:	
+		if is_fizz(x):
+			response = "fizz"
+		elif is_buzz(x):
+			response = "buzz"
+		else:
+			x += 1
+			response = str(x)
 	return response
 
 @app.route('/query/<int:q>', methods=['GET'])
 def get_reply(q):
 	reply = find_response(q)
 	return jsonify({'reply': reply})
+
+@app.route("/")
+def hello():
+    return "Bonjour!"
 
 
 if __name__ == "__main__":
